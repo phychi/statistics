3471769a6fcb8e235bbf350f64518fc478a652c1
Avanish Salunke <avanishsalunke16@gmail.com>
2026-02-19 17:33:33 +0530

    Handle NaN values as group labels (#393)

--- inst/anova1.m
+++ inst/anova1.m
@@ -146,15 +146,15 @@ function [p, anovatab, stats] = anova1 (x, group, displayopt, vartype)
     error ("anova1: GROUP must be a vector with the same number of rows as x.");
   endif
 
-  ## Identify NaN values (if any) and remove them from X along with
-  ## their corresponding values from group vector
-  nonan = ! isnan (x);
-  x = x(nonan);
-  group = group(nonan, :);
-
-  ## Convert group to indices and separate names
+  ## Convert group to indices and separate names first
   [group_id, group_names] = grp2idx (group);
   group_id = group_id(:);
+  x = x(:); 
+
+  ## identify NaN values in x or missing/empty categories in the group and remove them.
+  valid_data = !isnan (x) & !isnan (group_id);
+  x = x(valid_data);
+  group_id = group_id(valid_data);
   named = 1;
 
   ## Center data to improve accuracy and keep uncentered data for plotting
@@ -360,3 +360,15 @@ endfunction
 %! assert (tbl{2,5}, 15.523192, 1e-6);
 %! assert (tbl{2,3}, 2, 0);
 %! assert (tbl{2,4}, 7.5786897, 1e-6);
+
+## testing handling of missing values in both data and grouping variables
+%!test
+%! y = [10; 20; 9999; NaN; 40; 50];
+%! g = [1; 1; NaN; 1; 2; 2];
+%! [p, tbl, stats] = anova1 (y, g, "off");
+%! assert (p, 0.051317, 1e-6);
+%! assert (tbl{2,5}, 18, 1e-6);      
+%! assert (tbl{2,3}, 1, 0);          
+%! assert (tbl{3,3}, 2, 0);         
+%! assert (tbl{4,3}, 3, 0);         
+%! assert (stats.n, [2, 2], 0);      
